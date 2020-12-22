5b3818a625faf64df51e1340eccf19753090e145
Gustavo Souza <GustavoSouzaAlves12@gmail.com>
2020-12-22 02:46:50 -0300

    class 20/76

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -9,10 +9,19 @@ import ParentComponent from './Components/ParentComponent'
 import StudyComponent from './Components/StudyComponent'
 import CondtionalRendering from './Components/16_Conditional_Rendering';
 import NameList from './Components/17_NameList'
+import Stylesheet from './Components/20_Stylesheets'
+import Inline from './Components/20_inline'
+import './Styles/appStyles.css'
+import Styles from './Styles/appStyles.module.css'
+
 
 function App() {
   return (
     <div className="App">
+      <h1 className="error">Error</h1>
+      <h1 className={Styles.sucess}>Sucess</h1>
+      <Inline/>
+      <Stylesheet primary={true}/>
       <NameList/>
       <CondtionalRendering/>
       <FunctionClick/>
